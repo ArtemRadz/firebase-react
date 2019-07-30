466e82dddeb03f3e47f8ca62224375aa50fb1c6a
artemradz <artemradzivon@gmail.com>
2019-07-30 18:52:09 +0300

    init project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./App.css";
+
+const App = () => (
+  <React.StrictMode>
+    <div className="App">
+      <header>
+        <h2>React and Firebase App</h2>
+      </header>
+    </div>
+  </React.StrictMode>
+);
+
+export default App;
