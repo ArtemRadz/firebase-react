89090d02c3d7af94114e7dbc2a67317da68deb0a
Artem Radzivon <artem.radzivon@sirma.bg>
2019-12-13 12:37:08 +0200

    add firebaseConfig

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,9 @@
+  export const firebaseConfig = {
+    apiKey: "AIzaSyB5Hd-yEG02VOXC4qm93-Oo02HliZEnH_A",
+    authDomain: "lunch-rush-978d2.firebaseapp.com",
+    databaseURL: "https://lunch-rush-978d2.firebaseio.com",
+    projectId: "lunch-rush-978d2",
+    storageBucket: "lunch-rush-978d2.appspot.com",
+    messagingSenderId: "431726398870",
+    appId: "1:431726398870:web:30a82fac7cf44a23"
+  };
